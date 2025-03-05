ba5b41b4bd7d1c2bea9f5ea73eece3d02cac95b4
Colin Eckert <colineckert10@gmail.com>
2025-03-05 06:47:49 -0800

    draw player and fix resource usage

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     print("Starting Asteroids!")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    player = Player(screen.get_width() / 2, screen.get_height() / 2)
     dt = 0
 
     while True:
@@ -13,9 +15,10 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
 
-    dt = clock.tick(FPS) / 1000
+        dt = clock.tick(FPS) / 1000
 
 
 if __name__ == "__main__":
