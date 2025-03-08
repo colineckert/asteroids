51d9668f1f788c4c04103d153da66b2b9ce25dd8
Colin Eckert <colineckert10@gmail.com>
2025-03-07 16:15:23 -0800

    split asteroids when shot

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ def main():
                 sys.exit()
             for shot in shots:
                 if shot.collides_with(asteroid):
-                    asteroid.kill()
+                    asteroid.split()
                     shot.kill()
 
         screen.fill("black")
